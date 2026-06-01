60f740cd4e1a05a3b2dad572c2f1e436bc9bbb4a
mouiz elmarini <344250@student.mboutrecht.nl>
2026-06-01 19:42:55 +0200

    head toegevoegd

---
+++ homepaginamaken.php
@@ -0,0 +1,16 @@
+<?php
+// Start eventueel een sessie voor loginfunctionaliteit
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="nl">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aurora Theater</title>
+
+    <!-- Koppeling naar CSS bestand -->
+    <link rel="stylesheet" href="css/style.css">
+</head>
