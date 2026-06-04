00d8857883ca7278686c2fd08acad16e76cfd795
mouiz elmarini <344250@student.mboutrecht.nl>
2026-06-04 22:55:01 +0200

    einde sprint 1

--- controllers/HomeController.php
+++ controllers/HomeController.php
@@ -2,6 +2,11 @@
 
 class HomeController {
     public function index() {
+        global $pdo;
+        
+        // Initialize the database connection to trigger the error page in case of downtime
+        require_once __DIR__ . '/../database/config.php';
+
         require_once __DIR__ . '/../views/home.view.php';
     }
 }
